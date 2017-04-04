d0c9a3187d8c48f3a48a3c7a30e5b4eb20f3a96f
Windows 10 Gamer <Windows 10 Gamer@DESKTOP-I9E85NO>
2017-04-04 10:06:31 +0700

    Second commit

--- src/main/java/business/Student.java
+++ src/main/java/business/Student.java
@@ -5,15 +5,18 @@ package business;
 public class Student {
     private int studentNo;
     private String studentName;
-    
+    private String studentAddress;
     public Student(){
         
     }
 
-    public Student(int studentNo, String studentName) {
+    public Student(int studentNo, String studentName, String studentAddress) {
         this.studentNo = studentNo;
         this.studentName = studentName;
+        this.studentAddress = studentAddress;
     }
+
+   
 
     public int getStudentNo() {
         return studentNo;
